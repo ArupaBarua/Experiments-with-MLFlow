2f91149c2b3963ea3857451ccb520f1777895fc7
Arupa <arupab362@gmail.com>
2026-01-06 22:57:16 +0600

    Updated till autolog

---
+++ src/autolog.py
@@ -0,0 +1,41 @@
+import mlflow
+from mlflow import sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+wine = load_wine()
+x = wine.data
+y = wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+max_depth = 10
+n_estimators = 15
+
+mlflow.autolog()
+mlflow.set_experiment('MLOps-Exp1')
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    plt.savefig('Confusion-matrix.png')
+
+    mlflow.log_artifact(__file__)
+
+    mlflow.set_tags({"Author":"Arupa", "Project": "Wine Classification"})
+
+    print(accuracy)
